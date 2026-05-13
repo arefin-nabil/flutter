e5846317157088716f5fd770d588173c280c9aca
Nurul Arefin Nabil <arefinnabil256@gmail.com>
2026-05-13 17:10:09 +0600

    form + validation

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'form_validation.dart';
 import 'home.dart';
 import 'text_input.dart';
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.dark,
       title: 'Class - 1',
-      home: input_txt(),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/form_validation.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+import 'Dasboard.dart';
+import 'Home2.dart';
+import 'gridV.dart';
+import 'home.dart';
+
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    TextEditingController phoneController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+            'asset/logo.png',
+            height: 250,
+            width: 250,
+          ),
+          Text('Login with phone and password'),
+          SizedBox(
+            height: 20,
+          ),
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text('phone number'),
+                  TextFormField(
+                    controller: phoneController,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        hintText: 'Phone number', border: OutlineInputBorder()),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter phone number';
+                      } else if (value.length != 11) {
+                        return 'Please enter correct number';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Password', border: OutlineInputBorder()),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter password';
+                      } else if (value.length <= 6) {
+                        return 'Password must be at least 6 characters';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                      width: 300,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Home()));
+
+                              // Navigator.pushReplacement(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //         builder: (context) => Home()));
+                            }
+                          },
+                          child: Text('Submit'))),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => GridV()));
+                      },
+                      child: Text('Home-2')),
+
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => Dashboard(phone: phoneController.text,)));
+                      },
+                      child: Text('Dashboard'))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
